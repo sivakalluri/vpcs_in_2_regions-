ea587a9ff0c6081b82c2d79d546ce9ce2384f736
siva <sivakalluri97@gmail.com>
2022-07-08 11:40:18 +0000

    updates output files for making use as remote state datasources

--- vpc_output.tf
+++ vpc_output.tf
@@ -30,6 +30,18 @@ output "nat_public_ips" {
   value       = module.vpc.nat_public_ips
 }
 
+# VPC Database Subnets
+output "database_subnets" {
+  description = "List of IDs of database subnets"
+  value       = module.vpc.database_subnets
+}
+
+# VPC Database Subnet Group Name
+output "db_subnet_group_name" {
+  description = "Name of DB Subnet Group"
+  value       = module.vpc.database_subnet_group_name
+}
+
 # VPC AZs
 output "azs" {
   description = "A list of availability zones spefified as argument to this module"
